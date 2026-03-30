485ac7e4d2df10b3af83ef7b5e4f6be6cb1008f9
GeraltSHEN <geraltshen@gmail.com>
2026-03-30 11:44:44 -0400

    approx lp exp

---
+++ scripts/job_eval_aug_dual2.sh
@@ -0,0 +1,25 @@
+#!/bin/sh -l
+# FILENAME:  job_eval_aug_dual2
+
+#SBATCH -A canli
+#SBATCH --nodes=1 --gpus-per-node=1
+#SBATCH --partition=a100-40gb
+#SBATCH --mem=50G
+#SBATCH --cpus-per-task=8
+#SBATCH --time=3-1:30:00
+#SBATCH --job-name 2_eval_aug_dual
+#SBATCH --output=joboutput/job_eval_aug_dual2.out
+
+module load anaconda
+conda activate opt-ml-env
+
+DATASETS=("set_cover")
+CFG_IDS=(64)
+
+for DATASET in "${DATASETS[@]}"; do
+  echo "** ${DATASET} dataset **"
+  for CFG in "${CFG_IDS[@]}"; do
+    echo "eval ${DATASET} cfg ${CFG}"
+    python eval_heuristics.py --dataset "${DATASET}" --cfg_idx "${CFG}"
+  done
+done
---
+++ scripts/job_train_aug_dual6.sh
@@ -0,0 +1,25 @@
+#!/bin/sh -l
+# FILENAME:  job_train_aug_dual6
+
+#SBATCH -A canli
+#SBATCH --nodes=1 --gpus-per-node=1
+#SBATCH --partition=a100-40gb
+#SBATCH --mem=50G
+#SBATCH --cpus-per-task=8
+#SBATCH --time=3-1:30:00
+#SBATCH --job-name 6_train_aug_dual
+#SBATCH --output=joboutput/job_train_aug_dual6.out
+
+module load anaconda
+conda activate opt-ml-env
+
+DATASETS=("set_cover")
+CFG_IDS=(68)
+
+for DATASET in "${DATASETS[@]}"; do
+  echo "** ${DATASET} dataset **"
+  for CFG in "${CFG_IDS[@]}"; do
+    echo "Training ${DATASET} cfg ${CFG}"
+    python train_heuristics.py --dataset "${DATASET}" --cfg_idx "${CFG}"
+  done
+done
